915e761d7f75dd67ae7b3f046df3af08745afc90
sneshev <steffal.n2@gmail.com>
2025-11-05 13:13:05 +0100

    ~main.c | minimal start

--- src/main.c
+++ src/main.c
@@ -6,14 +6,15 @@
 /*   By: stefuntu <stefuntu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:27:15 by mmisumi           #+#    #+#             */
-/*   Updated: 2025/11/05 13:10:39 by stefuntu         ###   ########.fr       */
+/*   Updated: 2025/11/05 13:12:55 by stefuntu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-int get_map(t_map *map) {
+int get_map(t_map *map, char *filename_raw) {
 	(void)map;
+	(void)filename_raw;
 
 	return (1);
 }
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 		return (1);
-	if (get_map(&map) == -1)
+	if (get_map(&map, argv[1]) == -1)
 		return (1);
 
 	return (0);
