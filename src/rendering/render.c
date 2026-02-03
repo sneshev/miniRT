6b2f8eadbf1efe1ceef406854efc964ed60d5051
John Doe <steffal.n2@gmail.com>
2026-02-03 19:01:21 +0100

    +cylinder normal

--- src/rendering/render.c
+++ src/rendering/render.c
@@ -67,22 +67,52 @@ t_vec3	random_point_in_unit_sphere(void)
 	}
 	return (point);
 }
+bool	is_on_same_plane(t_vec3 *p1, t_vec3 *p2, t_vec3 *normal)
+{
+	float	d = dot(normalize(*p1 - *p2), normalize(*p1 - *normal));
+	if (d < T_MIN && d > -T_MIN)
+		return (true);
+	return (false);
+}
 
+t_vec3	get_cy_normal(t_cylinder *cy, t_vec3 *hitpoint)
+{
+		t_vec3		p;
+		p = cy->center + (cy->unit_dir * (cy->height / 2));
+		if (is_on_same_plane(&p, hitpoint, &cy->unit_dir)) {
+			return (cy->unit_dir);
+		}
+		p = cy->center - (cy->unit_dir * (cy->height / 2));
+		if (is_on_same_plane(&p, hitpoint, &cy->unit_dir)) {
+			return (-cy->unit_dir);
+		}
+		else
+		{
+		    t_vec3	cp = *hitpoint - cy->center;
+		    float	h = dot(cp, cy->unit_dir);
+		    t_vec3	axis_point = cy->center + cy->unit_dir * h;
+		    return (normalize(*hitpoint - axis_point));
+		}
+}
+
+//	doesnt account for hitting back of object yet (needs to flip normal)
 t_vec3	get_normal(t_object *object, t_vec3 hitpoint)
 {
-	t_vec3	normal;
+	t_vec3	normal = (t_vec3){0, 0, 0};
 
-	normal = (t_vec3){0.0f, 0.0f, 0.0f};
 	if (object->type == SPHERE) {
-		t_sphere *sph = (t_sphere *)object;
-		normal = normalize(hitpoint - sph->center);
+		normal = normalize(hitpoint - ((t_sphere *)object)->center);
 	}
 	else if (object->type == PLANE)
 	{
-		t_plane *pl = (t_plane *)object;
-		normal = pl->normal;
+		normal = ((t_plane *)object)->normal;
+	// if (dot(normal, ray.dir) > 0)
+	    // normal = normal * -1;
 	}
-	// get the normal for cylinder
+	else if (object->type == CYLINDER)
+	{
+		normal = get_cy_normal((t_cylinder *)object, &hitpoint);
+	}
 	return (normal);
 }
 
