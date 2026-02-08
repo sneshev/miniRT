2912d2a596e6097d9f753a793fcab83ee207725a
Mitani <mtimisumi@gmail.com>
2026-02-08 16:39:13 +0100

    to debug

--- src/rendering/intersect_light.c
+++ src/rendering/intersect_light.c
@@ -6,7 +6,7 @@
 /*   By: winnitytrinnity <winnitytrinnity@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 18:37:07 by winnitytrin       #+#    #+#             */
-/*   Updated: 2026/01/31 16:22:45 by winnitytrin      ###   ########.fr       */
+/*   Updated: 2026/02/05 16:10:32 by winnitytrin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,26 @@
 bool	intersect_light(t_ray *ray, t_object *obj)
 {
 	t_light	*light = (t_light *)obj;
-	t_vec3	op;
-	float	d;
-	float	t;
 
-	op = (light->origin - ray->origin);
-	t = dot(op, ray->unit_dir);
-	op = normalize(light->origin - ray->origin);
-	d = dot(op, ray->unit_dir);
-	if (fabs(1.0f - d) > 0.001f)
-		return (false);
-	if (t < ray->closest_t && t > T_MIN)
-	{
-		ray->closest_t = t;
-		ray->object = obj;
-		return (true);
+	t_vec3	q = ray->origin - light->origin;
+	float	a = 1;
+	float	b = dot(q, ray->unit_dir);
+	float	c = dot(q, q) - T_MIN;
+	float	disc = b*b - a*c;
+
+	if (disc > 0) {
+		float t = (-b - sqrt(disc));
+		if (t < ray->closest_t && t > T_MIN) {
+			ray->closest_t = t;
+			ray->object = obj;
+			return (true);
+		}
+		t = (-b + sqrt(disc));
+		if (t < ray->closest_t && t > T_MIN) {
+			ray->closest_t = t;
+			ray->object = obj;
+			return (true);
+		}
 	}
 	return (false);
 }
