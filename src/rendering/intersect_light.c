c3587670e1fe4113fcdc03ee2cdb5a637d1651e6
John Doe <steffal.n2@gmail.com>
2026-02-09 17:04:10 +0100

    about to do some structuring

--- src/rendering/intersect_light.c
+++ src/rendering/intersect_light.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   intersect_light.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: winnitytrinnity <winnitytrinnity@studen    +#+  +:+       +#+        */
+/*   By: sneshev <sneshev@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 18:37:07 by winnitytrin       #+#    #+#             */
-/*   Updated: 2026/02/05 16:10:32 by winnitytrin      ###   ########.fr       */
+/*   Updated: 2026/02/09 17:03:28 by sneshev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,20 @@
 bool	intersect_light(t_ray *ray, t_object *obj)
 {
 	t_light	*light = (t_light *)obj;
+	t_vec3	op;
+	float	d;
+	float	t;
 
-	t_vec3	q = ray->origin - light->origin;
-	float	a = 1;
-	float	b = dot(q, ray->unit_dir);
-	float	c = dot(q, q) - T_MIN;
-	float	disc = b*b - a*c;
-
-	if (disc > 0) {
-		float t = (-b - sqrt(disc));
-		if (t < ray->closest_t && t > T_MIN) {
-			ray->closest_t = t;
-			ray->object = obj;
-			return (true);
-		}
-		t = (-b + sqrt(disc));
-		if (t < ray->closest_t && t > T_MIN) {
-			ray->closest_t = t;
-			ray->object = obj;
-			return (true);
-		}
+	op = (light->origin - ray->origin);
+	t = dot(op, ray->unit_dir);
+	op = normalize(light->origin - ray->origin);
+	d = dot(op, ray->unit_dir);
+	if (fabs(1.0f - d) > 0.001f)
+		return (false);
+	if (t < ray->closest_t && t > T_MIN)
+	{
+		ray->closest_t = t;
+		ray->object = obj;
 	}
-	return (false);
+		return (true);
 }
