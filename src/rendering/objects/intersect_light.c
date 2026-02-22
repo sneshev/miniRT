b6f8bff743fa60514579353e54327065938ef6f3
John Doe <steffal.n2@gmail.com>
2026-02-22 13:40:38 +0100

    intersect_light() independent function

--- src/rendering/objects/intersect_light.c
+++ src/rendering/objects/intersect_light.c
@@ -6,7 +6,7 @@
 /*   By: sneshev <sneshev@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 18:37:07 by winnitytrin       #+#    #+#             */
-/*   Updated: 2026/02/21 18:05:37 by sneshev          ###   ########.fr       */
+/*   Updated: 2026/02/22 13:40:11 by sneshev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,37 @@
 
 bool	intersect_light(t_ray *ray, t_object *obj)
 {
-	t_sphere	sphere;
-	t_light		*light;
+	t_light	*l = (t_light *)obj;
+	t_vec3	q = ray->origin - l->origin;
+	float	b = dot(q, ray->unit_dir);
+	float	c = dot(q, q) - LIGHT_RADIUS*LIGHT_RADIUS;
+	float	disc = b*b - c;
 
-	light = (t_light *)obj;
-	sphere.center = light->origin;
-	sphere.radius = LIGHT_RADIUS;
-	sphere.albedo = light->emission;
-	if (intersect_sph(ray, (t_object *)&sphere) == true)
-	{
-		ray->hit.type = LIGHT;
-		return (true);
+	if (disc > 0) {
+		float t = (-b - sqrtf(disc));
+		if (t < ray->closest_t && t > T_MIN) {
+			ray->closest_t = t;
+			ray->hit.type = LIGHT;
+			ray->hit.albedo = l->emission;
+			ray->hit.hitpoint = ray->origin + ray->unit_dir * t;
+			if (length(ray->origin - l->origin) > LIGHT_RADIUS)
+				ray->hit.normal = normalize(ray->hit.hitpoint - l->origin);
+			else
+				ray->hit.normal = normalize(l->origin - ray->hit.hitpoint);
+			return (true);
+		}
+		t = (-b + sqrtf(disc));
+		if (t < ray->closest_t && t > T_MIN) {
+			ray->closest_t = t;
+			ray->hit.type = LIGHT;
+			ray->hit.albedo = l->emission;
+			ray->hit.hitpoint = ray->origin + ray->unit_dir * t;
+			if (length(ray->origin - l->origin) > LIGHT_RADIUS)
+				ray->hit.normal = normalize(ray->hit.hitpoint - l->origin);
+			else
+				ray->hit.normal = normalize(l->origin - ray->hit.hitpoint);
+			return (true);
+		}
 	}
 	return (false);
 }
-// bool	intersect_light(t_ray *ray, t_object *obj)
-// {
-// 	t_light	*light;
-// 	t_vec3	q;
-// 	float	b;
-// 	float	c;
-// 	float	disc;
-// 	float	t;
-
-// 	light = (t_light *)obj;
-// 	q = ray->origin - light->origin;
-// 	b = dot(q, ray->unit_dir);
-// 	c = dot(q, q) - LIGHT_RADIUS*LIGHT_RADIUS;
-// 	disc = b*b - c;
-
-// 	if (disc > 0)
-// 	{
-// 		t = (-b - sqrtf(disc));
-// 		if (t < ray->closest_t && t > T_MIN)
-// 		{
-// 			ray->hit.type = LIGHT;
-// 			return (true);
-// 		}
-// 		t = (-b + sqrtf(disc));
-// 		if (t < ray->closest_t && t > T_MIN)
-// 		{
-// 			ray->hit.type = LIGHT;
-// 			return (true);
-// 		}
-// 	}
-// 	return (false);
-// }
