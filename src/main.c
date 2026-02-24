f9888c6563cd7484fc1048ee4b80218f38cc56a4
John Doe <steffal.n2@gmail.com>
2026-02-24 12:56:39 +0100

    norm: main.c

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: sneshev <sneshev@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:27:15 by mmisumi           #+#    #+#             */
-/*   Updated: 2026/02/22 13:12:36 by sneshev          ###   ########.fr       */
+/*   Updated: 2026/02/24 12:56:30 by sneshev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
 	t_data	data;
 
@@ -21,26 +21,11 @@ int main(int argc, char *argv[])
 	data.scene.objs = make_dynamic_array(3, sizeof(t_objs));
 	if (!data.scene.objs)
 		return (1);
-
 	if (valid_input(argv[1], &data.scene) == false)
 		return (free_dynamic_array(&data.scene.objs), 1);
-
 	if (!init_minilibx(&data))
 		return (free_data_exit(&data, 1), 1);
-
 	render(&data.mlx, &data.scene);
 	free_dynamic_array(&data.scene.objs);
 	return (0);
 }
-
-/*	
-	.check for .rt extension
-	
-	.add to t_ray:
-		? t_vec3 hitpoint; 
-		? bool	 hit;
-*/
-
-
-
-
