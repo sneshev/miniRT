c691ff95f6db8bb3c709d695d99ccda6d03883a2
John Doe <steffal.n2@gmail.com>
2026-02-24 16:21:12 +0100

    norming: intersect_cy.c

--- src/rendering/intersections/intersect_cy.c
+++ src/rendering/intersections/intersect_cy.c
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intersect_cy.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sneshev <sneshev@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 16:15:23 by sneshev           #+#    #+#             */
+/*   Updated: 2026/02/24 16:18:32 by sneshev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
 bool	solve_quadratic(float a, float b, float c, float roots[2]);
 
-static void	fill_ray_info(t_plane *pl, t_ray *ray, float t) {
+static void	fill_ray_info(t_plane *pl, t_ray *ray, float t)
+{
 	ray->closest_t = t;
 	ray->hit.type = CYLINDER;
 	ray->hit.hitpoint = ray->origin + ray->unit_dir * t;
@@ -10,9 +23,11 @@ static void	fill_ray_info(t_plane *pl, t_ray *ray, float t) {
 	ray->hit.normal = pl->normal;
 }
 
-static bool	hits_cap(t_plane *pl, t_ray ray, float disc_radius, float *t) {
+static bool	hits_cap(t_plane *pl, t_ray ray, float disc_radius, float *t)
+{
 	(void)disc_radius;
-	if (intersect_pl(&ray, (t_object *)pl)) {
+	if (intersect_pl(&ray, (t_object *)pl))
+	{
 		*t = ray.closest_t;
 		pl->normal = ray.hit.normal;
 		if (length(ray.hit.hitpoint - pl->point) < disc_radius)
@@ -21,23 +36,25 @@ static bool	hits_cap(t_plane *pl, t_ray ray, float disc_radius, float *t) {
 	return (false);
 }
 
-bool	hits_caps(t_cylinder *cy, t_ray *ray) {
+bool	hits_caps(t_cylinder *cy, t_ray *ray)
+{
 	t_plane	pl;
 	float	t;
-	bool	ret = false;
+	bool	ret;
 
 	pl.albedo = cy->albedo;
 	pl.point = cy->center + (cy->unit_dir * cy->height * 0.5f);
 	pl.normal = cy->unit_dir;
-
-	if (hits_cap(&pl, *ray, cy->radius, &t)) {
+	ret = false;
+	if (hits_cap(&pl, *ray, cy->radius, &t))
+	{
 		fill_ray_info(&pl, ray, t);
 		ret = true;
 	}
-
 	pl.normal *= -1;
 	pl.point = cy->center - (cy->unit_dir * cy->height * 0.5f);
-	if (hits_cap(&pl, *ray, cy->radius, &t) /*t > T_MIN*/) {
+	if (hits_cap(&pl, *ray, cy->radius, &t))
+	{
 		fill_ray_info(&pl, ray, t);
 		ret = true;
 	}
@@ -83,35 +100,35 @@ bool	hits_side(t_cylinder *cy, t_ray *ray)
 
 	if (!hits_2d(cy, ray, &t, ray->origin - cy->center))
 		return (false);
-
 	hitpoint = ray->origin + ray->unit_dir * t;
 	height = dot(hitpoint - cy->center, cy->unit_dir);
-
 	if (height < -cy->height * 0.5f || height > cy->height * 0.5f)
 		return (false);
 	ray->closest_t = t;
 	ray->hit.type = CYLINDER;
 	ray->hit.hitpoint = ray->origin + ray->unit_dir * t;
 	ray->hit.albedo = cy->albedo;
-    t_vec3 cp = hitpoint - cy->center;
+    t_vec3 cp = hitpoint - cy->center;										/*could put in seperate get_normal() function*/
     t_vec3 axis_point = cy->center + cy->unit_dir * dot(cp, cy->unit_dir);
-	ray->hit.normal = normalize(axis_point - hitpoint ); // points outward
-	if (dot(ray->unit_dir, ray->hit.normal) > 0) 
-		ray->hit.normal *= -1; // points inward
+	ray->hit.normal = normalize(axis_point - hitpoint);
+	if (dot(ray->unit_dir, ray->hit.normal) > 0)
+		ray->hit.normal *= -1;
 	return (true);
 }
 
 bool	intersect_cyl(t_ray *ray, t_object *obj)
 {
-	t_cylinder *cy;
+	t_cylinder	*cy;
 	bool		hit;
 
 	hit = false;
 	cy = (t_cylinder *)obj;
-	if (hits_caps(cy, ray)) {
+	if (hits_caps(cy, ray))
+	{
 		hit = true;
 	}
-	if (hits_side(cy, ray)) {
+	if (hits_side(cy, ray))
+	{
 		hit = true;
 	}
 	return (hit);
