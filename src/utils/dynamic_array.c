4e0b3933caf1ce91517243696639a6a258e0d7cd
John Doe <steffal.n2@gmail.com>
2026-02-24 13:06:18 +0100

    norm: utils/ (but need to finish dynamic_array.c later)

--- src/utils/dynamic_array.c
+++ src/utils/dynamic_array.c
@@ -3,26 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   dynamic_array.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: winnitytrinnity <winnitytrinnity@studen    +#+  +:+       +#+        */
+/*   By: sneshev <sneshev@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 16:13:48 by mmisumi           #+#    #+#             */
-/*   Updated: 2026/01/25 15:57:12 by winnitytrin      ###   ########.fr       */
+/*   Updated: 2026/02/24 13:05:45 by sneshev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dynamicarray.h"
 /* create ft_realloc */
-/* fix ft_memcpy err */
 
 #include <string.h>
 
 #define DARR_EXPAN_ADD 4
 #define DARR_EXPAN_MUL 1
 
-
 static t_vec_meta	*access_meta(void *arr)
 {
-	return ((t_vec_meta *)arr) - 1;
+	return (((t_vec_meta *)arr) - 1);
 }
 
 size_t	get_count(void	*arr_ptr)
@@ -47,7 +45,7 @@ void	*make_dynamic_array(size_t in_capacity, size_t el_size)
 {
 	t_vec_meta	*meta;
 	void		*arr;
-	
+
 	if (in_capacity < 1)
 		in_capacity = 1;
 	meta = malloc(sizeof(t_vec_meta) + in_capacity * el_size);
@@ -68,11 +66,9 @@ int	reallocate_array(t_vec_meta **meta_p, void **arr)
 	meta = *meta_p;
 	meta->capacity = meta->capacity * DARR_EXPAN_MUL + DARR_EXPAN_ADD;
 	new_size = sizeof(t_vec_meta) + meta->capacity * meta->elem_size;
-
-	meta = realloc(meta, new_size);
+	meta = realloc(meta, new_size); // here!
 	if (!meta)
 		return (-1);
-
 	*meta_p = meta;
 	*arr = (void *)(meta + 1);
 	return (1);
@@ -81,7 +77,7 @@ int	reallocate_array(t_vec_meta **meta_p, void **arr)
 int	push(void *arr_ptr, void *elem, size_t el_size)
 {
 	t_vec_meta	*meta;
-	void 		**arr;
+	void		**arr;
 
 	arr = (void **)arr_ptr;
 	meta = ((t_vec_meta *)(*arr)) - 1;
@@ -94,7 +90,6 @@ int	push(void *arr_ptr, void *elem, size_t el_size)
 	if (el_size <= 0)
 		el_size = meta->elem_size;
 	ft_memcpy((char *)(*arr) + meta->count * meta->elem_size, elem, el_size);
-	// ft_memcpy((char *)(*arr) + meta->count * meta->elem_size, elem, meta->elem_size);
 	meta->count++;
 	return (1);
 }
